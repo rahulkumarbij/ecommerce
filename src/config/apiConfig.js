01a60834f85ebad1c92e83adf9158452b4b01ba0
rahulkumarbij <rahulkumarbijarniya123@gmail.com>
2024-09-09 20:30:26 +0530

    change api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,14 +1,13 @@
-import axios from "axios"
+import axios from "axios";
 
-export const API_BASE_URL="http://localhost:8080"
+export const API_BASE_URL = "https://ecommerce-server-wqq4.onrender.com/";
 
-const jwt=localStorage.getItem("jwt")
+const jwt = localStorage.getItem("jwt");
 
-
-export const api=axios.create({
-  baseURL:API_BASE_URL,
-  headers:{
-    "Authorization":`Bearer ${jwt}`,
-    "Content-Type":"application/json"
-  }
-})
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${jwt}`,
+    "Content-Type": "application/json",
+  },
+});
