5d8145ecab57e24ea45a91eeb4d54d929303037e
rahulkumarbij <rahulkumarbijarniya123@gmail.com>
2024-10-21 19:18:34 +0530

    change api in backand

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://ecommerce-server-wqq4.onrender.com/";
-
+export const API_BASE_URL = "http://localhost:8080";
 const jwt = localStorage.getItem("jwt");
 
 export const api = axios.create({
